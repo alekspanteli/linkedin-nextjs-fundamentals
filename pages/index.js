a065d1acca6c1c18683ffe088d9375832af2acee
alekspanteli <alekspanteli@rambler.ru>
2022-10-24 06:23:17 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="">
       <h1 className="text-lg text-blue-700 font-bold">
-        Welcome to my eLearning!!!
+        Welcome to my eLearning!!!!!!!
       </h1>
     </div>
   );
