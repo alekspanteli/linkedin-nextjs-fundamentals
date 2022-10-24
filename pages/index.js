fd88859954a1f3ebed1ea8f94e02f3d2af967d0c
alekspanteli <alekspanteli@rambler.ru>
2022-10-24 14:04:14 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const [query, getQuery] = useState();
+  const router = useRouter();
+  const handleOnChange = (e) => getQuery(e.target.value);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/news/${query}`);
+  };
   return (
     <div className="">
       <h1 className="text-lg text-blue-700 font-bold">
         Welcome to my eLearning!!!!!!!
       </h1>
+
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          className="border rounded-md w-full px-3 py-2"
+          onChange={handleOnChange}
+        />
+      </form>
     </div>
   );
 }
